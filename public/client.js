6b3db622671c128b7e8981d2f9e10a177a5d2f0e
Glitch (skitter-place) <none>
2018-06-07 18:34:43 +0000

    🏊🏼🏥 Checkpoint ./public/client.js:1174538/80 ./views/index.html:1174538/284

--- public/client.js
+++ public/client.js
@@ -1,10 +1,10 @@
 // client-side js
 // run by the browser each time your view template is loaded
-  var socket=io.connect(process.env.PORT);
+  
 
 (function(){
   console.log('hello world :o');
   
-
+var socket=io.connect(process.env.PORT);
   
 })()
